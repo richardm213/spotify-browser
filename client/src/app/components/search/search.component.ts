7d65d8ef9bdce9c28b720317813d8f43eade4c8d
Richard Meng <lemonbuttercookie@gmail.com>
2021-11-11 23:19:27 -0800

    Completed search, carousel, and track-list components

--- client/src/app/components/search/search.component.ts
+++ client/src/app/components/search/search.component.ts
@@ -1,29 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { SpotifyService } from '../../services/spotify.service';
-import { ArtistData } from '../../data/artist-data';
-import { AlbumData } from '../../data/album-data';
-import { TrackData } from '../../data/track-data';
-import { ResourceData } from '../../data/resource-data';
+import { Component, OnInit } from "@angular/core";
+import { SpotifyService } from "../../services/spotify.service";
+import { ArtistData } from "../../data/artist-data";
+import { AlbumData } from "../../data/album-data";
+import { TrackData } from "../../data/track-data";
+import { ResourceData } from "../../data/resource-data";
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css'],
-  providers: [ SpotifyService ]
+  selector: "app-search",
+  templateUrl: "./search.component.html",
+  styleUrls: ["./search.component.css"],
+  providers: [SpotifyService],
 })
 export class SearchComponent implements OnInit {
-  searchString:string;
-  searchCategory:string = 'artist';
-  searchCategories:string[] = ['artist', 'album', 'track'];
-  resources:ResourceData[];
+  searchString: string;
+  searchCategory: string = "artist";
+  searchCategories: string[] = ["artist", "album", "track"];
+  resources: ResourceData[];
 
-  constructor(private spotifyService:SpotifyService) { }
+  constructor(private spotifyService: SpotifyService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   search() {
     //TODO: call search function in spotifyService and parse response
+    this.resources = null;
+    this.spotifyService
+      .searchFor(this.searchCategory, this.searchString)
+      .then((r) => {
+        this.resources = r;
+      });
   }
-
+  // clear() {
+  //   this.searchString = "";
+  //   this.searchCategory = "artist";
+  // }
 }
